9760c5d883bf48ed90958036b2ab7cc3ec5123f6
byarti <chinarireddy2005@gmail.com>
2026-08-08 00:58:31 +0530

    Update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
-import { Splash } from './screens/Splash';
 import { Home } from './screens/Home';
 import { Login } from './screens/Login';
 import { OtpVerification } from './screens/OtpVerification';
+import { ProviderDashboard } from './screens/ProviderDashboard';
 import { ServiceDetails } from './screens/ServiceDetails';
 import { DateTimeSelection } from './screens/DateTimeSelection';
 import { AddressScreen } from './screens/AddressScreen';
 import { BookingReview } from './screens/BookingReview';
 import { BookingSuccess } from './screens/BookingSuccess';
 import { CreateAccount } from './screens/CreateAccount';
-import { Layout } from './components/Layout';
 
 function App() {
   const location = useLocation();
@@ -19,18 +18,25 @@ function App() {
   return (
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Splash />} />
+        {/* Default route points directly to Client Booking Experience */}
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        
+        {/* Authentication Sheet (Dual Role Client & Provider Login) */}
         <Route path="/login" element={<Login />} />
         <Route path="/otp" element={<OtpVerification />} />
+
+        {/* Provider Partner SaaS Dashboard & Catalog / Schedule Manager */}
+        <Route path="/provider" element={<ProviderDashboard />} />
+        <Route path="/dashboard" element={<ProviderDashboard />} />
+
+        {/* Secondary / Booking Flow Routes */}
         <Route path="/service" element={<ServiceDetails />} />
         <Route path="/date-time" element={<DateTimeSelection />} />
         <Route path="/address" element={<AddressScreen />} />
         <Route path="/review" element={<BookingReview />} />
         <Route path="/success" element={<BookingSuccess />} />
         <Route path="/create-account" element={<CreateAccount />} />
-        <Route element={<Layout />}>
-          <Route path="/home" element={<Home />} />
-        </Route>
       </Routes>
     </AnimatePresence>
   );
