6402097b65cc1e6f3e44758971125a29f6a9a93a
byarti <chinarireddy2005@gmail.com>
2026-08-17 23:22:55 +0530

    Update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,51 +1,62 @@
 import React from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
+
+// Screens
 import { ClientHome } from './screens/ClientHome';
 import { ProviderStorefront } from './screens/ProviderStorefront';
-import { Login } from './screens/Login';
-import { OtpVerification } from './screens/OtpVerification';
 import { ProviderDashboard } from './screens/ProviderDashboard';
-import { ServiceDetails } from './screens/ServiceDetails';
-import { DateTimeSelection } from './screens/DateTimeSelection';
-import { AddressScreen } from './screens/AddressScreen';
+import { Login } from './screens/Login';
 import { BookingReview } from './screens/BookingReview';
 import { BookingSuccess } from './screens/BookingSuccess';
-import { CreateAccount } from './screens/CreateAccount';
-import { Splash } from './screens/Splash';
+import { DateTimeSelection } from './screens/DateTimeSelection';
+import { AddressScreen } from './screens/AddressScreen';
+
+// Global Modals & Notifications
+import { AuthModal } from './components/common/AuthModal';
+import { LocationModal } from './components/common/LocationModal';
+import { CartDrawer } from './components/common/CartDrawer';
+import { BookingModal } from './components/common/BookingModal';
+import { Toast } from './components/common/Toast';
 
 function App() {
   const location = useLocation();
 
   return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        {/* Client Discovery Discovery Platform */}
-        <Route path="/" element={<ClientHome />} />
-        <Route path="/home" element={<ClientHome />} />
-        <Route path="/splash" element={<Splash />} />
-        
-        {/* Provider Storefront Experience */}
-        <Route path="/storefront" element={<ProviderStorefront />} />
-        <Route path="/storefront/:providerId" element={<ProviderStorefront />} />
+    <div className="bg-[#FFFFFF] min-h-screen text-[#111111] antialiased selection:bg-[#111111] selection:text-white">
+      {/* Central Global Modals and Notification Components */}
+      <AuthModal />
+      <LocationModal />
+      <CartDrawer />
+      <BookingModal />
+      <Toast />
 
-        {/* Authentication Sheet (Dual Role Client & Provider Login) */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/otp" element={<OtpVerification />} />
+      {/* Routes */}
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          {/* Client Discovery Layer */}
+          <Route path="/" element={<ClientHome />} />
+          <Route path="/home" element={<ClientHome />} />
 
-        {/* Provider Partner SaaS Dashboard & Catalog / Schedule Manager */}
-        <Route path="/provider" element={<ProviderDashboard />} />
-        <Route path="/dashboard" element={<ProviderDashboard />} />
+          {/* Provider Public Storefront Experience */}
+          <Route path="/storefront" element={<ProviderStorefront />} />
+          <Route path="/storefront/:providerId" element={<ProviderStorefront />} />
 
-        {/* Secondary / Booking Flow Routes */}
-        <Route path="/service" element={<ServiceDetails />} />
-        <Route path="/date-time" element={<DateTimeSelection />} />
-        <Route path="/address" element={<AddressScreen />} />
-        <Route path="/review" element={<BookingReview />} />
-        <Route path="/success" element={<BookingSuccess />} />
-        <Route path="/create-account" element={<CreateAccount />} />
-      </Routes>
-    </AnimatePresence>
+          {/* Provider Turnkey SaaS Dashboard */}
+          <Route path="/provider" element={<ProviderDashboard />} />
+          <Route path="/dashboard" element={<ProviderDashboard />} />
+
+          {/* Authentication Route */}
+          <Route path="/login" element={<Login />} />
+
+          {/* Booking & Review Secondary Routes */}
+          <Route path="/date-time" element={<DateTimeSelection />} />
+          <Route path="/address" element={<AddressScreen />} />
+          <Route path="/review" element={<BookingReview />} />
+          <Route path="/success" element={<BookingSuccess />} />
+        </Routes>
+      </AnimatePresence>
+    </div>
   );
 }
 
