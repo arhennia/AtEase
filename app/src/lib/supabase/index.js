ab3ec265c8fb01c5308310d839035ab046c2472e
arhennia <chinarireddy2005@gmail.com>
2026-09-15 22:54:48 +0530

    Create index.js

---
+++ app/src/lib/supabase/index.js
@@ -0,0 +1,35 @@
+export { supabase, isSupabaseConfigured } from './client';
+
+export {
+  toE164,
+  signInWithGoogle,
+  sendPhoneOtp,
+  verifyPhoneOtp,
+  signUpWithEmail,
+  signInWithEmail,
+  signOutUser,
+  getAuthUser,
+} from './auth';
+
+export { fetchProfile, ensureProfile } from './profiles';
+export { mapBrandOwnerFromDb, normalizeAppointment } from './mappers';
+
+export {
+  fetchBrandOwnerByUserId,
+  fetchBrandOwnerBySlug,
+  hydratePartner,
+  createBrandOwnerRecord,
+  updateBrandOwnerRecord,
+} from './brandOwners';
+
+export { fetchPrimarySalon, createSalonRecord } from './salons';
+export { fetchServicesByOwnerId, createServicesRecords } from './services';
+export { fetchAppointmentsByOwnerId, createAppointmentRecord } from './appointments';
+export { fetchClientsByOwnerId, fetchAnalyticsByOwnerId } from './clients';
+
+/** @deprecated Use signUpWithEmail */
+export { signUpWithEmail as signUpProvider } from './auth';
+/** @deprecated Use signInWithEmail */
+export { signInWithEmail as signInProvider } from './auth';
+/** @deprecated Use signOutUser */
+export { signOutUser as signOutProvider } from './auth';
