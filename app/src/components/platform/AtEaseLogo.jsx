ae30ed399681754a02f7ad5322b85bbf1e25fb7e
arhennia <chinarireddy2005@gmail.com>
2026-09-16 01:16:25 +0530

    Create AtEaseLogo.jsx

---
+++ app/src/components/platform/AtEaseLogo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function AtEaseLogo({ className = 'text-[1.4rem] sm:text-[1.65rem]' }) {
+  return (
+    <span className={`noto-serif-grantha-regular leading-none ${className}`}>
+      AtEase
+    </span>
+  );
+}
